9f5de6a54347bf7dced96ce2d1bec7687fab5625
John Laffey <laffeychem@gmail.com>
2024-12-20 23:24:19 -0500

    add stripe webhook for payment processing

---
+++ myshop/payment/webhook.py
@@ -0,0 +1,32 @@
+import stripe
+from django.conf import settings
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import stripe.error
+from orders.models import Order
+
+
+@csrf_exempt
+def stripe_webhook(request):
+    payload = request.body
+    sig_headers = request.META["HTTP_STRIPE_SIGNATURE"]
+    event = None
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_headers, settings.STRIPE_WEBHOOK_SECRET
+        )
+    except ValueError as e:
+        return HttpResponse(status=400)
+    except stripe.error as e:
+        return HttpResponse(status=400)
+    if event.type == "checkout.session.completed":
+        session = event.data.object
+        if session.mode == "payment" and session.payment_status == "paid":  # type: ignore
+            try:
+                order = Order.objects.get(id=session.client_reference_id)  # type: ignore
+            except Order.DoesNotExist:
+                return HttpResponse(status=404)
+            order.paid = True
+            order.stripe_id = session.payment_intent  # type: ignore
+            order.save()
+    return HttpResponse(status=200)
